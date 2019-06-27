4cd61f7895ba339889dce17d6c4b4d8e81a719f6
nokox <koxbox@gmail.com>
2019-06-27 23:07:11 +0300

    setup

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -6,12 +6,12 @@ import numpy as np
 from PIL import Image
 from io import BytesIO
 
-
 model = ClassPredictor()
 
 
+# функция, которая возвращает полное наименование гриба (лат.+рус.)
 def get_full_name(classname):
-    return shrooms_dict[classname]+' ('+classname+')'
+    return shrooms_dict[classname] + ' (' + classname + ')'
 
 
 def send_prediction_on_photo(bot, update):
@@ -27,18 +27,27 @@ def send_prediction_on_photo(bot, update):
 
     class_, prob_ = model.predict(image_stream)
 
+    # создадим кнопки для получения доп. информации
+    keyboard = [[telegram.InlineKeyboardButton("Инфо о грибе",
+                                               url='https://ru.wikipedia.org/wiki/'
+                                               + str(class_).replace(' ', '_')),
+                 telegram.InlineKeyboardButton("Другие фото",
+                                               url='https://www.google.com/search?q='
+                                               + str(class_).replace(' ', '+') + '&tbm=isch')]]
+    reply_markup = telegram.InlineKeyboardMarkup(keyboard)
+
     # теперь отправим результат
     bot.sendMessage(chat_id=update.message.chat_id,
                     text='<b>' + get_full_name(str(class_)) + '</b>, вероятность: {}%'.format(prob_),
-                    parse_mode='HTML')
+                    parse_mode='HTML',
+                    reply_markup=reply_markup)
+
     print("Sent Answer to user, predicted: {}".format(class_))
 
 
 def start(bot, update):
-    # подробнее об объекте update: https://core.telegram.org/bots/api#update
     bot.sendMessage(chat_id=update.message.chat_id, text=reply_texts['hello'], parse_mode='Markdown')
     bot.sendMessage(chat_id=update.message.chat_id, text=reply_texts['guide'])
-    bot.sendMessage(chat_id=update.message.chat_id, text=reply_texts['warning'])
 
 
 def shrooms_list(bot, update):
@@ -46,7 +55,6 @@ def shrooms_list(bot, update):
 
 
 def conversation(bot, update):
-    # bot.send_message(chat_id=update.message.chat_id, text=last_prediction)
     bot.send_message(chat_id=update.message.chat_id, text=reply_texts['reg_answer'])
 
 
@@ -55,7 +63,8 @@ def unknown(bot, update):
 
 
 if __name__ == '__main__':
-    from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+    from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, CallbackQueryHandler
+    import telegram
     import logging
 
     # Включим самый базовый логгинг, чтобы видеть сообщения об ошибках
@@ -64,20 +73,14 @@ if __name__ == '__main__':
         level=logging.INFO)
 
     updater = Updater(token=token)
-    updater.dispatcher.add_handler(MessageHandler(Filters.photo, send_prediction_on_photo))
-
-    start_handler = CommandHandler('start', start)  # этот обработчик реагирует только на команду /start
-    list_handler = CommandHandler('list', shrooms_list)  # этот обработчик реагирует только на команду /list
-    echo_handler = MessageHandler(Filters.text, conversation)  # реагирует на любой текст
-    unknown_handler = MessageHandler(Filters.command, unknown)
-
 
     # регистрируем обработчики
-    updater.dispatcher.add_handler(start_handler)
-    updater.dispatcher.add_handler(list_handler)
-    updater.dispatcher.add_handler(echo_handler)
-    updater.dispatcher.add_handler(unknown_handler)
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.photo, send_prediction_on_photo))
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('list', shrooms_list))
+    dp.add_handler(MessageHandler(Filters.text, conversation))
+    dp.add_handler(MessageHandler(Filters.command, unknown))
+    dp.add_handler(CallbackQueryHandler(menu_actions))
 
     updater.start_polling()  # start the bot
-
-
